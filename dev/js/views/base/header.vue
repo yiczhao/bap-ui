4044e5af2d0ba678b347505c0fe166db1b132bc7
ZhengJie <zhengjie@kashuo.com>
2016-12-21 18:52:41 +0800

    BAMS-46页面：公用头尾菜单

--- dev/js/views/base/header.vue
+++ dev/js/views/base/header.vue
@@ -1,12 +1,26 @@
 <template>
     <div class="header">
-        头部模板
-        <a @click="loginOut">退出</a>
+        <div class="header-title">银行活动管理系统</div>
+        <div class="header-infor">
+            <div class="img-area">
+                <img src="../../../img/username.png" alt="">
+            </div>
+            <div class="information-show">
+                <span>建设银行江西分行</span>
+                <span>信用卡部</span>
+            </div>
+            <div class="function-click">
+                <a @click="loginOut">退出</a>
+            </div>
+        </div>
     </div>
 </template>
 <script>
     export default{
         created(){
+            if(!sessionStorage.getItem('loginList')){
+                this.$router.go('login');
+            }
         },
         data(){
             return{
@@ -17,7 +31,7 @@
                 this.$http.post('./user/logout').then((data)=>{
                     if(data.data.code===0){
                         sessionStorage.clear();
-                        this.$router.go({'name':'login'});
+                        this.$router.go('login');
                     }
                     else{
                         dialog('error',data.data.message)
