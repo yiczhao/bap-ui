0c1beabef0fcb699aadefbc3a7990384292ccf56
ZhengJie <zhengjie@kashuo.com>
2016-12-27 10:21:40 +0800

    BAMS-108前端：活动分析页面

---
+++ dev/js/views/activity/activity-analysis.vue
@@ -0,0 +1,35 @@
+<template>
+	<div class="activity-analysis">
+		<div class="activity-search">
+			<div>
+				<label></label>
+				<input type="" name="">
+			</div>
+			<div></div>
+		</div>
+		<div class="transaction-data"></div>
+		<div class="transaction-area"></div>
+		<div class="transaction-time"></div>
+		<div class="Merchant-data"></div>
+		<div class="cardBIN-data"></div>
+		<div class="one-card"></div>
+		<div class="red-bage"></div>
+		<div class="latinos-data"></div>
+		<div class="latinos-probability"></div>
+	</div>
+</template>
+<script type="text/javascript">
+	export default{
+		data(){
+			return{
+
+			}
+		},
+		methods:{
+
+		},
+		ready(){
+
+		}
+	}
+</script>
