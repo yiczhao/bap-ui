181e1f969aad47a23412c40ce0d136c312a841cc
ZhaoJunHan <zhaojunhan@kashuo.com>
2017-01-11 16:50:42 +0800

    BAMS-102页面：删除活动

--- dev/js/ajax/activity/activity_manage_model.js
+++ dev/js/ajax/activity/activity_manage_model.js
@@ -13,6 +13,15 @@ function model(_this) {
         getList(data) {
             return _this.$http.post('./transfer/activity_configure/api/v1/activity/list',data)
         },
+        /**
+         * @description 活动列表查询
+         * @param data
+         * @returns {*}
+         */
+        deleteActivity(_id) {
+            return formDataRequest('./transfer/activity_configure/api/v1/activity/delete/'+_id).get()
+        },
+
     }
 }
 module.exports = model
--- dev/js/views/activity/activity-manage.vue
+++ dev/js/views/activity/activity-manage.vue
@@ -68,7 +68,7 @@
                     <a v-if="n.step==1" @click="setProp(n.propes)" v-link="{name:'basic-rule',params:{'activityId':n.id,'rulename':n.ruleType}}">编辑</a>
                     <a v-if="n.step==2" @click="setProp(n.propes)" v-link="{name:n.ruleType,params:{'ruleId':n.id}}">编辑</a>
                     <a v-if="n.step==3" @click="setProp(n.propes)" v-link="{name:'bussiness-set',params:{'bactivityId':n.id}}">编辑</a>
-                    <a>删除</a>
+                    <a @click="deleteActivity(n.id)">删除</a>
                 </td>
             </tr>
             <tr v-show="!searchList.length">
@@ -131,6 +131,14 @@
                     if(res.data.code===0){
                         this.$set('searchList',res.data.data);
                         this.searchDate.total=res.data.total;
+                    }
+                })
+            },
+            deleteActivity(_id){
+                this.model.deleteActivity(_id).then((res)=>{
+                    if(res.data.code===0){
+                        dialog('successTime','已删除')
+                        this.getList();
                     }
                 })
             }
