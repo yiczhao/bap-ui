4bb540433db2c9ea3adb4181a76b94139c00ad20
ZhengJie <zhengjie@kashuo.com>
2017-01-12 15:45:34 +0800

    BAMS-109前端：交易查询页面

--- dev/js/filters/filter_phone.js
+++ dev/js/filters/filter_phone.js
@@ -2,10 +2,10 @@
  * [phone格式 过滤]
  * @param  {[type]} input     [输入值]
  */
-export default function (str){
+export default function (){
 	// return  val.replace(/(\d{3})\d{4}(\d{4})/,"$1*****$3")
 	// return  val.replace(/(\d{3})(\d{5})(\d{3})/,"$1*****$3")
-	var reg = /^(\d{4})\d+(\d{4})$/;
-		str = str.replace(reg, "$1****$2");
-	return  str
+	// var reg = /^(\d{4})\d+(\d{4})$/;
+	// 	str = val.replace(reg, "$1****$2");
+	// return  str
 }
