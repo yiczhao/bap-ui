ed2af3df2845616c792d50e418d1476b0d7bf3ff
ZhengJie <zhengjie@kashuo.com>
2017-03-23 10:27:06 +0800

    BAMS-493页面优化：活动分析之详情页 BAMS-494页面优化：活动分析之效果预览页面

--- dev/js/routers.js
+++ dev/js/routers.js
@@ -249,7 +249,9 @@ export default function (Vue,router){
                 /* 活动分析数据总览 */
                 'activity-analysis': {
                     name: 'activity-analysis',
-                    router_type:'activity',
+                     router_type:[
+                        {link:'activity-analysis',text:'活动分析'}
+                    ],
                     router_tile:'活动分析',
                     component: (resolve) => {
                         require(['./views/activity/activity-analysis.vue'], resolve)
@@ -258,7 +260,10 @@ export default function (Vue,router){
                 /* 活动分析页面 */
                 'activity-data-overview/:adoActivityId/:mainStepChance': {
                     name: 'activity-data-overview',
-                    router_type:'activity',
+                    router_type:[
+                        {link:'activity-analysis',text:'活动分析'},
+                        {link:'activity-data-overview',text:'活动分析-表格总览'}
+                    ],
                     router_tile:'活动分析',
                     component: (resolve) => {
                         require(['./views/activity/activity-data-overview.vue'], resolve)
@@ -267,7 +272,10 @@ export default function (Vue,router){
                 /* 活动分析页面pdf导出页面 */
                 'activity-pdfout/:pdfActivityId': {
                     name: 'activity-pdfout',
-                    router_type:'activity',
+                    router_type:[
+                        {link:'activity-analysis',text:'活动分析'},
+                        {link:'activity-pdfout',text:'活动分析-导出PDF'}
+                    ],
                     router_tile:'导出PDF',
                     component: (resolve) => {
                         require(['./views/activity/activity-pdfout.vue'], resolve)
