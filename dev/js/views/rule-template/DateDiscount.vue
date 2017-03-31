e156aa794993ef78253ae4e11b45f6d711f03efa
ZhengJie <zhengjie@kashuo.com>
2017-03-31 18:48:58 +0800

    BAMS-645【活动管理】操作列置灰操作没对齐BAMS-589【几号几折】活动规则折扣值未限制

--- dev/js/views/rule-template/DateDiscount.vue
+++ dev/js/views/rule-template/DateDiscount.vue
@@ -7,7 +7,7 @@
         </div>
     </span>
     <span>号</span>
-    <input class="input padding-input" type="text" v-model="n.discount" v-limitaddprice="n.discount"/>
+    <input class="input padding-input" type="text" v-model="n.discount" v-limitaddprice="n.discount" @blur="checkDiscount(index)"/>
     <span class="margin-span">折</span>
     <i v-if="index===0" class="icon-add" @click="addLine">新增一条</i>
     <i v-if="index!==0" class="icon-remove" @click="submitdata.splice(index, 1)">删除一条</i>
@@ -150,7 +150,10 @@
             },
             addLine(){
                 this.submitdata.push({'date':'','discount':'','show':false})
-            }
+            },
+            checkDiscount(index){
+                if(this.submitdata[index].discount>10){dialog('info','折扣不能大于10！')};
+            },
         },
         events:{
             getData(){
