2b355d407ce88c2adf3bee9b8472003ade542689
zj <zhangjin@kashuo.com>
2017-04-10 12:07:29 +0800

    BAMS-679【活动详情】权益使用时间获取信息错误

--- dev/js/views/activity/activity-info.vue
+++ dev/js/views/activity/activity-info.vue
@@ -24,8 +24,8 @@
                 <div class="activity-val">{{basicData.uuid | get_bank uuidsList}}</div>
             </div>
             <!--<div class="mian-row">-->
-                <!--<div class="activity-type">创建时间</div>-->
-                <!--<div class="activity-val">2017-01-01 00:00:00</div>-->
+            <!--<div class="activity-type">创建时间</div>-->
+            <!--<div class="activity-val">2017-01-01 00:00:00</div>-->
             <!--</div>-->
         </div>
         <div class="info-title">
@@ -37,12 +37,12 @@
             </ul>
         </div>
         <div class="info-main">
-        <!--<div class="main-title">-->
+            <!--<div class="main-title">-->
             <!--<span class="activity-type">活动名称 /</span><span> {{basicData.name}}</span>-->
             <!--<span v-if="!!ruleList.ruleName" class="activity-type">活动形式 /</span><span> {{ruleList.ruleName}}</span>-->
             <!--<span class="activity-type">创建时间 /</span><span> {{basicData.createdAt}}</span>-->
             <!--<span class="activity-type">所属银行 /</span><span>{{basicData.uuid | get_bank uuidsList}}</span>-->
-        <!--</div>-->
+            <!--</div>-->
             <div v-show="step===1" class="info-basic">
                 <div class="main-row">
                     <div class="row-right">
@@ -60,8 +60,8 @@
                 </div>
                 <div class="main-row">
                     <div class="row-right">
-                    <span class="activity-type">参与时间段 /</span>
-                    <span class="activity-val">每天参与时间段<template v-for="n in basicData.timesList">【{{n}}】</template></span>
+                        <span class="activity-type">参与时间段 /</span>
+                        <span class="activity-val">每天参与时间段<template v-for="n in basicData.timesList">【{{n}}】</template></span>
                     </div>
                     <div class="row-right">
                         <span class="activity-type">所属银行 /</span>
@@ -87,68 +87,68 @@
             </div>
             <div v-show="step===2" class="info-rule">
                 <div class="main-row table-row">
-                <span class="w140 activity-type">{{ruleList.ruleName}} /</span>
-                <template v-if="ruleList.ruleType=='MeetMinus'">
+                    <span class="w140 activity-type">{{ruleList.ruleName}} /</span>
+                    <template v-if="ruleList.ruleType=='MeetMinus'">
                     <span class="activity-val" v-for="n in ruleList[ruleList.ruleTypes]">
                     满{{n.meetMoney}}元，减{{n.minusMoney}}元.
                     </span>
-                </template>
-                <template v-if="ruleList.ruleType=='EveryMeetMinus'">
-                    <span class="activity-val">每满{{ruleList[ruleList.ruleTypes].meetMoney}}元，减{{ruleList[ruleList.ruleTypes].minusMoney}}元.</span>
-                </template>
-                <template v-if="ruleList.ruleType=='ImmediatelyMinus'">
-                    <span class="activity-val">立减{{ruleList[ruleList.ruleTypes].money}}元.</span>
-                </template>
-                <template v-if="ruleList.ruleType=='RandomMinus'">
+                    </template>
+                    <template v-if="ruleList.ruleType=='EveryMeetMinus'">
+                        <span class="activity-val">每满{{ruleList[ruleList.ruleTypes].meetMoney}}元，减{{ruleList[ruleList.ruleTypes].minusMoney}}元.</span>
+                    </template>
+                    <template v-if="ruleList.ruleType=='ImmediatelyMinus'">
+                        <span class="activity-val">立减{{ruleList[ruleList.ruleTypes].money}}元.</span>
+                    </template>
+                    <template v-if="ruleList.ruleType=='RandomMinus'">
                     <span class="activity-val" v-for="n in ruleList[ruleList.ruleTypes]">
                     随机立减{{n.amount}}元，{{n.number}}名.
                     </span>
-                </template>
-                <template v-if="ruleList.ruleType=='MeetDiscount'">
+                    </template>
+                    <template v-if="ruleList.ruleType=='MeetDiscount'">
                     <span class="activity-val" v-for="n in ruleList[ruleList.ruleTypes]">
                     满{{n.meetMoney}}元，打{{n.discount}}折.
                     </span>
-                </template>
-                <template v-if="ruleList.ruleType=='Ticket'">
+                    </template>
+                    <template v-if="ruleList.ruleType=='Ticket'">
                     <span class="activity-val" v-for="n in ruleList[ruleList.ruleTypes]">
                     票务名称{{n.name}}，原价{{n.originalPrice}}元，实付{{n.actualPayment}}元.
                     单次每卡可购{{n.numberLimit}}张
                     </span>
-                </template>
-                <template v-if="ruleList.ruleType=='SerialDiscount'">
+                    </template>
+                    <template v-if="ruleList.ruleType=='SerialDiscount'">
                     <span class="activity-val" v-for="n in ruleList[ruleList.ruleTypes]">
                     用户刷卡{{n.belowMoney}}以内，第{{n.time}}次，可享受{{n.discount}}折.
                     </span>
-                </template>
-                <template v-if="ruleList.ruleType=='WeekdayDiscount'">
+                    </template>
+                    <template v-if="ruleList.ruleType=='WeekdayDiscount'">
                     <span class="activity-val" v-for="n in ruleList[ruleList.ruleTypes]">
                     每周{{n.weekday}}，打{{n.discount }}折.
                     </span>
-                </template>
-                <template v-if="ruleList.ruleType=='DateDiscount'">
+                    </template>
+                    <template v-if="ruleList.ruleType=='DateDiscount'">
                     <span class="activity-val" v-for="n in ruleList[ruleList.ruleTypes]">
                     {{n.date}}号，立减{{n.discount}}折.
                     </span>
-                </template>
-                <template v-if="ruleList.ruleType=='RandomDiscount'">
+                    </template>
+                    <template v-if="ruleList.ruleType=='RandomDiscount'">
                     <span class="activity-val" v-for="n in ruleList[ruleList.ruleTypes]">
                     随机{{n.discount}}折，{{n.number}}名.
                     </span>
-                </template>
-                <template v-if="ruleList.ruleType=='CouponDiscount'">
+                    </template>
+                    <template v-if="ruleList.ruleType=='CouponDiscount'">
                     <span class="activity-val" v-for="n in ruleList[ruleList.ruleTypes]">
                     {{n.belowMoney}}元以内, 打{{n.discount}}折优惠.
                     </span>
-                </template>
-                <template v-if="ruleList.ruleType=='CouponMinus'">
+                    </template>
+                    <template v-if="ruleList.ruleType=='CouponMinus'">
                     <span class="activity-val" class="activity-val" v-for="n in ruleList[ruleList.ruleTypes]">
                     优惠劵名称:{{n.name}}，抵扣{{n.deductibleMoney}}元， 每次可使用{{n.number}}张.
                     </span>
-                </template>
-            </div>
+                    </template>
+                </div>
                 <div v-show="!!ruleList.moneys.length" class="main-row table-row">
-                <span class="activity-type">金额限制 /</span>
-                <span class="activity-val" v-for="n in ruleList.moneys">
+                    <span class="activity-type">金额限制 /</span>
+                    <span class="activity-val" v-for="n in ruleList.moneys">
                     <template v-if="n.type =='minimum_consume'">
                     最低消费金额:{{n.amount}}元.
                     </template>
@@ -159,7 +159,7 @@
                     多少元内参与打折:{{n.amount}}元.
                     </template>
                 </span>
-            </div>
+                </div>
                 <div v-show="!!ruleList.quantities.length" class="main-row table-row">
                     <span class="activity-type">参与次数限制 /</span>
                     <span class="activity-val" v-for="n in ruleList.quantities">
@@ -185,14 +185,14 @@
                 <div class="store-title">共{{storeList.length}}家商户</div>
                 <table class="table">
                     <tr>
-                    <th>商户ID</th>
-                    <th>商户名称</th>
-                    <th>地区</th>
+                        <th>商户ID</th>
+                        <th>商户名称</th>
+                        <th>地区</th>
                     </tr>
                     <tr v-for="n in storeList">
-                    <td>{{n.storeId}}</td>
-                    <td>{{n.storeName}}</td>
-                    <td>{{n.industry}}</td>
+                        <td>{{n.storeId}}</td>
+                        <td>{{n.storeName}}</td>
+                        <td>{{n.industry}}</td>
                     </tr>
                 </table>
             </div>
@@ -208,7 +208,7 @@
                     </div>
                     <div class="row-right">
                         <span class="activity-type">使用时间 /</span>
-                        <span class="activity-val" v-if="!!equityData"> {{validPeriod[equityData.validPeriod]}}</span>
+                        <span class="activity-val" v-if="!!validPeriod"> {{validPeriod}}</span>
                     </div>
                 </div>
                 <div class="main-row">
@@ -220,12 +220,12 @@
                         <span class="activity-type">权益每天数量 /</span>
                         <span class="activity-val" v-if="!!equityData"> {{equityData.totalOneDay}}</span>
                     </div>
-                        <div class="row-right" v-if="!!equityData">
-                            <template v-if="!!equityData.times">
+                    <div class="row-right" v-if="!!equityData">
+                        <template v-if="!!equityData.times">
                             <span class="activity-type">活动定时抢 /</span>
                             <span class="activity-val" v-if="!!equityData">{{equityTimeStr}}</span>
-                            </template>
-                        </div>
+                        </template>
+                    </div>
                 </div>
                 <div class="main-row table-row">
                     <div class="row-right">
@@ -237,47 +237,47 @@
             </div>
         </div>
     </div>
-        <!--<div class="activity-baseinfo">-->
-            <!--<span class="act-title">活动基本信息</span>-->
-            <!--<div class="baseinfo-boby">-->
-                <!--<div class="mian-row">-->
-                    <!--<div class="activity-type">活动名称</div>-->
-                    <!--<div class="activity-val">8.8折抵用券</div>-->
-                <!--</div>-->
-                <!--<div class="mian-row">-->
-                    <!--<div class="activity-type">活动预算</div>-->
-                    <!--<div class="activity-val">50000</div>-->
-                <!--</div>-->
-                <!--<div class="mian-row">-->
-                    <!--<div class="activity-type">活动时间</div>-->
-                    <!--<div class="activity-val">2017-01-01 00:00:00-2017-01-01 00:00:00</div>-->
-                <!--</div>-->
-                <!--<div class="mian-row">-->
-                    <!--<div class="activity-type">所属银行</div>-->
-                    <!--<div class="activity-val">江西银行</div>-->
-                <!--</div>-->
-                <!--<div class="mian-row">-->
-                    <!--<div class="activity-type">参与时间段</div>-->
-                    <!--<div class="activity-val">每天参与时间段【10:00-20:00】</div>-->
-                <!--</div>-->
-                <!--<div class="mian-row">-->
-                    <!--<div class="activity-type">活动主题</div>-->
-                    <!--<div class="activity-val">1、江西银行</div>-->
-                <!--</div>-->
-            <!--</div>-->
-        <!--</div>-->
+    <!--<div class="activity-baseinfo">-->
+    <!--<span class="act-title">活动基本信息</span>-->
+    <!--<div class="baseinfo-boby">-->
+    <!--<div class="mian-row">-->
+    <!--<div class="activity-type">活动名称</div>-->
+    <!--<div class="activity-val">8.8折抵用券</div>-->
+    <!--</div>-->
+    <!--<div class="mian-row">-->
+    <!--<div class="activity-type">活动预算</div>-->
+    <!--<div class="activity-val">50000</div>-->
+    <!--</div>-->
+    <!--<div class="mian-row">-->
+    <!--<div class="activity-type">活动时间</div>-->
+    <!--<div class="activity-val">2017-01-01 00:00:00-2017-01-01 00:00:00</div>-->
+    <!--</div>-->
+    <!--<div class="mian-row">-->
+    <!--<div class="activity-type">所属银行</div>-->
+    <!--<div class="activity-val">江西银行</div>-->
+    <!--</div>-->
+    <!--<div class="mian-row">-->
+    <!--<div class="activity-type">参与时间段</div>-->
+    <!--<div class="activity-val">每天参与时间段【10:00-20:00】</div>-->
+    <!--</div>-->
+    <!--<div class="mian-row">-->
+    <!--<div class="activity-type">活动主题</div>-->
+    <!--<div class="activity-val">1、江西银行</div>-->
+    <!--</div>-->
+    <!--</div>-->
+    <!--</div>-->
 </template>
 
 <!--<template>-->
-    <!--<div class="activity-info">-->
-        <!--<div class="info-title">-->
-            <!--<ul class="tab-bor">-->
-                <!--<li @click="step=1" :class="{'active':step===1}">活动基本信息</li>-->
-                <!--<li v-show="!!ruleList.ruleType" @click="step=2" :class="{'active':step===2}">规则设置</li>-->
-                <!--<li v-show="!!storeList.length" @click="step=3" :class="{'active':step===3}">商户信息</li>-->
-                <!--&lt;!&ndash;<li @click="step=4" :class="{'active':step===4}">权益信息</li>&ndash;&gt;-->
-            <!--</ul>-->
-        <!--</div>-->
+<!--<div class="activity-info">-->
+<!--<div class="info-title">-->
+<!--<ul class="tab-bor">-->
+<!--<li @click="step=1" :class="{'active':step===1}">活动基本信息</li>-->
+<!--<li v-show="!!ruleList.ruleType" @click="step=2" :class="{'active':step===2}">规则设置</li>-->
+<!--<li v-show="!!storeList.length" @click="step=3" :class="{'active':step===3}">商户信息</li>-->
+<!--&lt;!&ndash;<li @click="step=4" :class="{'active':step===4}">权益信息</li>&ndash;&gt;-->
+<!--</ul>-->
+<!--</div>-->
 
 <!--</template>-->
 <script type="text/javascript">
@@ -314,16 +314,7 @@
                     '%DAYLEFT%':'当天剩余名额',
                     '%TOTALLEFT%':'总剩余名额'
                 },
-                validPeriod:{
-                        '-1':'与活动时间同步',
-                        '0':'用户得到权益1天内',
-                        '1':'用户得到权益2天内',
-                        '2':'用户得到权益3天内',
-                        '3':'用户得到权益5天内',
-                        '4':'用户得到权益10天内',
-                        '5':'用户得到权益15天内',
-                        '6':'用户得到权益30天内'
-                },
+                validPeriod:'',
                 equityData:{},
                 equityTimeStr:'',
                 ruleStr:'',
@@ -344,6 +335,19 @@
                 {
                     let favorId=this.equityData.favorId;
                     this.getActInfo(favorId);
+                    let validPeriod='';
+                    if(this.equityData.validPeriod!=null)
+                    {
+                        validPeriod=this.equityData.validPeriod;
+                        if(validPeriod==-1)
+                        {
+                            this.validPeriod='与活动时间同步';
+                        }
+                        else
+                        {
+                            this.validPeriod='用户得到权益'+validPeriod+'天内';
+                        }
+                    }
                     if(this.equityData.times!=null&&this.equityData.weeks!=null)
                     {
                         let equityTimeStr='';
