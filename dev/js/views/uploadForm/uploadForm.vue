a6537c88f0a66fef70e77434b75a69d991d3f9e9
周俭 <zhoujian@kashuo.com>
2017-09-01 16:05:46 +0800

    新增工具页面

--- dev/js/views/uploadForm/uploadForm.vue
+++ dev/js/views/uploadForm/uploadForm.vue
@@ -8,10 +8,55 @@
                 </tr>
                 <tr>
                     <td>
-                        <input class="input" type="text" v-model="cardNumber" @keyup.enter="submit" placeholder="请输入银行卡号"/>
+                        <input class="input" type="text" v-model="activityIds" @keyup.enter="submit" placeholder="请输入活动ID(逗号隔开)"/>
+                        同步大后台活动交易数据(逗号隔开)
                     </td>
                     <td>
                         <a @click="submit">确认</a>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <input class="input" type="text" v-model="startDate" @keyup.enter="delSub" placeholder="请输入起始日期"/>
+                        删除交易数据（起始日期后无交易数据的，该活动的交易都删除）yyyy-MM-dd
+                    </td>
+                    <td>
+                        <a @click="delSub">确认</a>
+                    </td>
+                </tr>
+                <tr>
+                    <td>删除非银行活动的交易
+                    </td>
+                    <td>
+                        <a @click="delTransSub">确认</a>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <input class="input" type="text" v-model="transNos" @keyup.enter="delByNos" placeholder="请输入交易流水号(逗号隔开)"/>
+                        按交易流水号删除交易（逗号隔开）
+                    </td>
+                    <td>
+                        <a @click="delByNos">确认</a>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <input class="input" type="text" v-model="transNo" placeholder="请输入交易流水号(逗号隔开)"/>
+                        <datepicker type="datetime" v-model="tradeTime" placeholder="请选择交易时间(yyyy-MM-dd HH:mm:ss)"></datepicker>
+                        交易时间修复(yyyy-MM-dd HH:mm:ss)
+                    </td>
+                    <td>
+                        <a @click="repaire">确认</a>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <input class="input" type="text" v-model="actIds" placeholder="请输入活动id(逗号隔开)"/>
+                        按活动统计：交易数据分析、交易时段分析、商户数据分析、交易区域分析、卡BIN数据分析、单卡交易分析
+                    </td>
+                    <td>
+                        <a @click="actidsCount">确认</a>
                     </td>
                 </tr>
             </table>
@@ -22,25 +67,57 @@
 	export default{
         data(){
             return{
-                cardNumber:''
+                activityIds:'',
+                startDate:'',
+                transNos:'',
+                transNo:'',
+                tradeTime:'',
+                actIds:''
             }
         },
         methods:{
             submit(){
-                let data={
-                    cardNumber:this.cardNumber
-                }
-                formDataRequest('./merchant_data/trade_count').get(data).then((res)=>{
-                    if(res.data.code===0){
-                        dialog('success','成功！');
-                    }
-                })
-                this.$http.post('./user/verify_message_code',data).then((res)=>{
+                this.$http.post('./tool/synTransData?activityIds='+this.activityIds).then((res)=>{
                     if(res.data.code===0){
                         dialog('success','成功！');
                     }
                 })
             },
+            delSub(){
+                this.$http.post('./tool/transaction/delete?startDate='+this.startDate).then((res)=>{
+                    if(res.data.code===0){
+                        dialog('success','成功！');
+                    }
+                })
+            },
+            delTransSub(){
+                this.$http.post('./tool/transaction/not/activity/delete').then((res)=>{
+                    if(res.data.code===0){
+                        dialog('success','成功！');
+                    }
+                })
+            },
+            delByNos(){
+                this.$http.post('./tool/transaction/delete/by/trans/nos?transNo='+this.transNos).then((res)=>{
+                    if(res.data.code===0){
+                        dialog('success','成功！');
+                    }
+                })
+            },
+            repaire(){
+                this.$http.post('./tool/tradeTime/repair?transNo='+this.transNos+'&tradeTime='+this.tradeTime).then((res)=>{
+                    if(res.data.code===0){
+                        dialog('success','成功！');
+                    }
+                })
+            },
+            actidsCount(){
+                this.$http.post('./tool/trade/statistics?activityIds='+this.actIds).then((res)=>{
+                    if(res.data.code===0){
+                        dialog('success','成功！');
+                    }
+                })
+            }
         },
         created(){
             if(JSON.parse(sessionStorage.getItem('loginList')).roleID==1){
